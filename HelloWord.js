c956a72ad3d097d5fe20e8df739090ef462e2470
JimmyLi <Jimmy.Li@acer.com>
2020-10-28 16:20:50 +0800

    test

--- HelloWord.js
+++ HelloWord.js
@@ -2,7 +2,7 @@
 const http = require('http');
 
 //設定server網址，因為在本機端測試，所以輸入127.0.0.1
-//const hostname = '127.0.0.1'  //上傳至伺服器需拿掉
+const hostname = '127.0.0.1'  //上傳至伺服器需拿掉
 
 //port 號會由 Heroku 給予，因此不再自行指定
 const port = process.env.PORT || 3000;
@@ -15,4 +15,4 @@ const server = http.createServer((req, res) => {
 });
 
 //監聽得到的 port 號開啟
-server.listen(port, () => console.log(`Listening on ${PORT}`));
+server.listen(port, () => console.log(`Listening on ${port}`));
